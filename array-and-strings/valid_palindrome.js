a31a1d5d7edaf79117a866b1c87ca189e94a139e
Rajat Gupta <rajatgupta@Rajats-MacBook-Pro-Touchbar.local>
2020-03-04 14:54:41 -0800

    Some basic js notes

---
+++ array-and-strings/valid_palindrome.js
@@ -0,0 +1,14 @@
+/*
+This is a two pointer problem. 
+Start one pointer from the beginning and one from the end of the string
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isPalindrome = function(s) {
+    
+};
+
