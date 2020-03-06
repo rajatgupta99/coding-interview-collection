88a958ac59120ad0c5f6f3a83c730d3f64f4d149
Rajat Gupta <rajatgupta@Rajats-MacBook-Pro-Touchbar.local>
2020-03-05 23:19:42 -0800

    updates

--- array-and-strings/valid_palindrome.js
+++ array-and-strings/valid_palindrome.js
@@ -1,4 +1,19 @@
 /*
+Question:
+Valid Palindrome
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+Input: "A man, a plan, a canal: Panama"
+Output: true
+
+Example 2:
+Input: "race a car"
+Output: false
+
+
 Helper Notes:
 This is a two pointer problem. 
 Start one pointer from the beginning and one from the end of the string
