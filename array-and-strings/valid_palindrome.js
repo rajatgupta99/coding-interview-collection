a451a9939b7d1769e42d2f8a4660c0f90b25b1cf
Rajat Gupta <rajatgupta@Rajats-MacBook-Pro-Touchbar.local>
2020-03-05 21:35:38 -0800

    Valid Palindrome js solutions

--- array-and-strings/valid_palindrome.js
+++ array-and-strings/valid_palindrome.js
@@ -9,6 +9,28 @@ Start one pointer from the beginning and one from the end of the string
  */
 
 var isPalindrome = function(s) {
-    
+    let start = 0, end = s.length - 1;
+    const regexExp = /^[0-9a-zA-Z]$/;
+
+    while(start < end){
+        if(!regexExp.test(s[start])){
+            start += 1;
+        }else if(!regexExp.test(s[end])){
+            end -= 1;
+        }else{
+            if(s[start].toLowerCase() != s[end].toLowerCase()){
+                return false
+            }else{
+                start += 1;
+                end -= 1;
+            }
+        }
+    }
+    return true;
 };
 
+let checkOne = "A man, a plan, a canal: Panama";
+console.log(isPalindrome(checkOne));
+
+let checkTwo = "race a car";
+console.log(isPalindrome(checkTwo));
